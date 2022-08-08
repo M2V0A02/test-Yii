9c341e3200294031b01c8aa5da77aea3e6364ce3
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-08-08 11:08:50 +0500

    Контент контроллера и подключение ассетов

--- views/layouts/basic.php
+++ views/layouts/basic.php
@@ -1,3 +1,8 @@
+<?php
+    use app\assets\AppAsset;
+    AppAsset::register($this);
+    $this->beginPage() 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,10 +10,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php $this->head(); ?>
 </head>
 <body>
-    
-    <h1>Hello, BASIC!</h1>
-
+    <?php $this->beginBody(); ?>
+        <h1>Hello, BASIC!</h1>
+        <?= $content ?>
+    <?php $this->endBody(); ?>
 </body>
 </html>
+<?php $this->endPage(); ?>
