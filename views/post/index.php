46533cc9a81e2efd0a24b1ca84c6a81e4cc6e6ff
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-08-08 11:45:56 +0500

    Изменить порядок загрузки для js файла

--- views/post/index.php
+++ views/post/index.php
@@ -1,3 +1,5 @@
 Контент
 
-<?php $this->registerJsFile('@web/js/main.js', ['depends' => 'yii\web\YiiAsset']) ?>
+<?php //$this->registerJsFile('@web/js/main.js', ['depends' => 'yii\web\YiiAsset'])
+        $this->registerJs('alert(1)', \yii\web\View::POS_LOAD);
+?>
