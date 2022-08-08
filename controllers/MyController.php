ad80ca22eaec00a202adce43911a933c00202ad1
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-08-08 10:30:04 +0500

    Глобальное подключение переменной

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -7,7 +7,7 @@ class MyController extends AppController {
     public function actionIndex($id = null) {
         $hi = "hello wordl";
         $names = ['Egor', 'Vlad', 'Mixail', 'Jeny'];
-        return \app\controllers\debug($hi);
+        debug($names);
         //return $this->debug($hi);
         // return $this->render('index', ["hello" => $hi, "names" => $names]);
         //return $this->render('index', compact('hi', 'names', 'id'));
