1e6da8f331306d85b8d87e0ccf060226c672e984
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-08-10 12:26:51 +0500

    Дополнительные возможности создание форм

--- views/post/index.php
+++ views/post/index.php
@@ -5,9 +5,9 @@ use yii\bootstrap4\Html;
 ?>
 
 <h1>Test Action</h1>
-<?php $form = ActiveForm::begin() ?>
-<?= $form->field($model, 'name')?>
-<?= $form->field($model, 'email')?>
-<?= $form->field($model, 'text')?>
+<?php $form = ActiveForm::begin(['options' => ['id' => 'testForm']]) ?>
+<?= $form->field($model, 'name')->label("имя")->passwordInput()?>
+<?= $form->field($model, 'email')->input('email')?>
+<?= $form->field($model, 'text')->label("текст сообщения")->textarea() ?>
 <?= Html::submitButton('Отправить', ['class' => 'btn btn-success'])?>
 <?php ActiveForm::end() ?>
