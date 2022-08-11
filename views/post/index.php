4d288c512c58e5366baf90f30ebddbde4c5ddebd
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-08-11 13:08:30 +0500

    установка без композера

--- views/post/index.php
+++ views/post/index.php
@@ -3,6 +3,7 @@
 use yii\bootstrap4\ActiveForm;
 use yii\bootstrap4\Html;
 use app\components\MyWidget;
+use mihaildev\ckeditor\CKEditor;
 // count($user->messages)
 // debug($messages)
 ?>
@@ -38,9 +39,17 @@ use app\components\MyWidget;
 
 <h1>Test Action</h1>
 <?php $form = ActiveForm::begin(['options' => ['id' => 'testForm']]) ?>
-<?= $form->field($model, 'message')?>
+<!-- //$form->field($model, 'message')?> -->
 <?= $form->field($model, 'user_id')?>
 <!-- composer require --prefer-dist yiisoft/yii2-jui -->
 <?= yii\jui\DatePicker::widget(['name' => 'attributeName']) ?>
+<?= 
+    $form->field($model, 'message')->widget(CKEditor::className(),[
+        'editorOptions' => [
+            'preset' => 'full', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
+            'inline' => false, //по умолчанию false
+        ],
+    ]);
+?>
 <?= Html::submitButton('Отправить', ['class' => 'btn btn-success'])?>
 <?php ActiveForm::end() ?>
